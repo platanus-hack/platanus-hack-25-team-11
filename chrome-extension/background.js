9dad591d0e9aa567fb2d3892d51b922a7c9e33af
Verner Codoceo <vacodoceo@gmail.com>
2025-11-23 01:50:57 -0300

    feat: change icon color on toggle

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -2,6 +2,77 @@
 
 let checkoutStatus = new Map();
 
+// Initialize icon state on startup
+chrome.runtime.onInstalled.addListener(() => {
+  updateIcon();
+});
+
+chrome.runtime.onStartup.addListener(() => {
+  updateIcon();
+});
+
+// Listen for storage changes to update icon
+chrome.storage.onChanged.addListener((changes, area) => {
+  if (area === 'local' && changes.thinkTwiceEnabled) {
+    updateIcon();
+  }
+});
+
+async function updateIcon() {
+  const result = await chrome.storage.local.get('thinkTwiceEnabled');
+  const isEnabled = result.thinkTwiceEnabled !== undefined ? result.thinkTwiceEnabled : true;
+
+  if (isEnabled) {
+    // Set to normal colored icon
+    chrome.action.setIcon({
+      path: {
+        16: 'icons/icon16.png',
+        48: 'icons/icon48.png',
+        128: 'icons/icon128.png'
+      }
+    });
+  } else {
+    // Convert to greyscale
+    await setGreyscaleIcon();
+  }
+}
+
+async function setGreyscaleIcon() {
+  const sizes = [16, 48, 128];
+  const imageData = {};
+
+  for (const size of sizes) {
+    try {
+      // Fetch the icon image
+      const response = await fetch(chrome.runtime.getURL(`icons/icon${size}.png`));
+      const blob = await response.blob();
+      const imageBitmap = await createImageBitmap(blob);
+
+      // Create canvas and draw image
+      const canvas = new OffscreenCanvas(size, size);
+      const ctx = canvas.getContext('2d');
+      ctx.drawImage(imageBitmap, 0, 0, size, size);
+
+      // Get image data and convert to greyscale
+      const imgData = ctx.getImageData(0, 0, size, size);
+      const pixels = imgData.data;
+
+      for (let i = 0; i < pixels.length; i += 4) {
+        const grey = pixels[i] * 0.299 + pixels[i + 1] * 0.587 + pixels[i + 2] * 0.114;
+        pixels[i] = grey;
+        pixels[i + 1] = grey;
+        pixels[i + 2] = grey;
+      }
+
+      imageData[size] = imgData;
+    } catch (error) {
+      console.error(`[Think twice] Error creating greyscale icon for size ${size}:`, error);
+    }
+  }
+
+  chrome.action.setIcon({ imageData });
+}
+
 // Listen for messages from content scripts
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.type === 'CHECKOUT_DETECTED') {
